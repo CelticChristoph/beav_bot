6bbb8468319dfef7443502d74b04eecd45293eb8
Christopher Carlsen <celticchristoph@live.com>
2018-11-14 07:26:54 -0800

    Minor tweak to respond with JSON until web page creation occurs.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 // GET home page.
 router.get('*', (req, res) => {
-  res.render('index');
+  // res.render('index');
+  res.status(200);
+  res.json({
+    message: 'Boo',
+  });
 });
 
 module.exports = router;
