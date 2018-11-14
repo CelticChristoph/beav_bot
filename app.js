fb8a79dee520e3361e3ca38e63ab67e6511e2ad0
Christopher Carlsen <celticchristoph@live.com>
2018-11-13 16:35:24 -0800

    Mongo DB stuff.

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const express = require('express');
 // Discord bot
 const disc = require('discord.js');
 
+// Mongo database
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient();
+
 // Router from ./routes
 const routes = require('./routes/index');
 
@@ -23,6 +27,17 @@ module.exports = app;
 const { token, ownerID } = JSON.parse(fs.readFileSync('./secret/config.json'));
 console.log(`Using Discord bot API token: ${token}`);
 console.log(`OwnerID is set as: ${ownerID}`);
+
+// ----- Mongo Init -----------------------------------------------------------
+const url = 'mongodb://localhost:27017/mydb'; // CHANGE
+
+console.log(typeof mongo); // DEBUG/TESTING
+
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  console.log('Database created!');
+  db.close();
+});
 
 // ----- Bot ------------------------------------------------------------------
 client.on('ready', () => {
