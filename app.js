e9713d65875420c0c5c5da755ac4833ef2934871
Christopher Carlsen <celticchristoph@live.com>
2018-11-14 08:49:27 -0800

    Updated message parsing for Discord client.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const assert = require('assert');
 const routes = require('./routes/index');
 
 
-// ----- Setup ----------------------------------------------------------------
+// ----- Page Setup -----------------------------------------------------------
 const app = express();
 const discClient = new disc.Client();
 
@@ -27,7 +27,7 @@ app.use('/', routes);
 module.exports = app;
 
 // ----- Bot Info -------------------------------------------------------------
-const { token, ownerID } = JSON.parse(fs.readFileSync('./secret/config.json'));
+const { token, ownerID, prefix } = JSON.parse(fs.readFileSync('./secret/config.json'));
 console.log(`Using Discord bot API token: ${token}`);
 console.log(`OwnerID is set as: ${ownerID}`);
 
@@ -35,9 +35,12 @@ console.log(`OwnerID is set as: ${ownerID}`);
 const { MongoClient } = mongo;
 const url = 'mongodb://localhost:27017'; // CHANGE
 const dbName = 'cludgeBot';
+const mdbConfig = {
+  useNewUrlParser: true,
+};
 
 // Create a new MongoClient
-const mdbClient = new MongoClient(url);
+const mdbClient = new MongoClient(url, mdbConfig);
 let mdb = null;
 
 // Use connect method to connect to the Server
@@ -51,24 +54,39 @@ mdbClient.connect((err) => {
   // mdbClient.close();
 });
 
-// ----- Bot ------------------------------------------------------------------
+// ----- Shared Functions------------------------------------------------------
+function addUser() {
+  mdb.collection('users').insertOne({ a: 1 }, (err, r) => {
+    assert.equal(null, err);
+    assert.equal(1, r.insertedCount);
+  });
+}
+
+// ----- Bot Commands ---------------------------------------------------------
+function parseCommand(msg) {
+  const split = msg.content.substr(1).split(' ');
+  const command = split[0];
+
+  if (command === 'ping') {
+    msg.reply('Pong!');
+  }
+
+  if (command === 'add') {
+    addUser();
+  }
+}
 discClient.on('ready', () => {
   console.log(`Logged in as ${discClient.user.tag}!`);
 });
 
 discClient.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.author === discClient.user) { return -1; }
+
+  if (msg.content.startsWith(prefix)) {
+    parseCommand(msg);
   }
+
+  return -1;
 });
 
-discClient.on('message', (msg) => {
-  if (msg.content === 'add user') {
-    mdb.collection('users').insertOne({ a: 1 }, (err, r) => {
-      assert.equal(null, err);
-      assert.equal(1, r.insertedCount);
-    });
-  }
-});
-
-// discClient.login(token);
+discClient.login(token);
